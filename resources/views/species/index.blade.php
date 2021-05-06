cded88650296cf8937b2e4d7601ef98d2be9c634
Margaret Marple <oldauntie@oldauntie.org>
2021-05-06 21:46:31 +0200

    test config

--- resources/views/species/index.blade.php
+++ resources/views/species/index.blade.php
@@ -13,6 +13,7 @@
                 </div>
 
                 <div class="card-body">
+                    
                     @if (session('status'))
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}

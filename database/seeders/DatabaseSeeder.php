754e5867ec6990931cfe675d0010ce6bce468f3f
onenone <rn@g>
2023-01-08 21:56:43 +0100

    Added Clinic scaffold

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,14 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
         $this->call(CountriesTableSeeder::class);
         $this->call(LocalesTableSeeder::class);
+
+        $this->call(ClinicsTableSeeder::class);
+
     }
 }

bcc37fcb32c64311b0562ac603493ef28b0ae9d8
onenone <rn@g>
2024-08-25 15:22:13 +0200

    feat: full working modal data-id to pass data

--- resources/views/esperimenti/index.blade.php
+++ resources/views/esperimenti/index.blade.php
@@ -36,27 +36,6 @@
                         here are some experiments [body]
 
 
-
-                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
-                            aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <input class="modalTextInput form-control" placeholder="Text Here" />
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="saveEdit btn btn-primary">Save changes</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
                         <table class="table table-bordered">
                             <tbody>
                                 <tr>
@@ -95,22 +74,18 @@
         </div>
     </div>
 
-
+    @include('esperimenti.modal')
 
     <script type="module">
         
 
         $(function() {
-            var io = $('#io').html();
-            console.log(io);
-            
             $('#exampleModal').on('show.bs.modal', function(e) {
                 $('.modalTextInput').val('');
                 let btn = $(e.relatedTarget); // e.related here is the element that opened the modal, specifically the row button
                 let id = btn.data('id'); // this is how you get the of any `data` attribute of an element
-                console.log('hi ...');
-                console.log(id);
-
+                console.log('raised show.bs.modal event from button with data-id=' + id);
+                
                 let text = $('.modalTextInput').val(id);
 
 
@@ -131,27 +106,5 @@
             console.log(text + ' --> ' + id);
         }
     </script>
-
-
-
-
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModalOriginal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    ... mah...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
-        </div>
-    </div>
 
 @endsection
---
+++ resources/views/esperimenti/modal.blade.php
@@ -0,0 +1,18 @@
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <input class="modalTextInput form-control" placeholder="Text Here" />
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary saveEdit">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
